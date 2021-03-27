0c0d800918205991835ed7318dae15c74b59ed37
Eric Cawthorne <eccawthorne@gmail.com>
2021-03-26 23:22:36 -0400

    Added handling of incorrect number of arguments

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #define SDL_MAIN_HANDLED
+#include <iostream>
 #include <thread>
 #include <chrono>
 #include "Chip8.h"
 #include "Screen.h"
 
+
 int main(int argc, char* argv[])
 {
+	if (argc != 2)
+	{
+		std::cout << "Incorrect number of arguments." << '\n'
+				  << "Correct syntax is \"RealChip8 <rom>\"";
+		return 1;
+	}
+
 	Chip8 myChip8;
-	Screen chip8Display(argv[1], 64, 32, 15);
 	if (!myChip8.loadRom(argv[1])) return 1;
+	Screen chip8Display(argv[1], 64, 32, 15);
 
 	int status = 0;
 	while(status != -1)
